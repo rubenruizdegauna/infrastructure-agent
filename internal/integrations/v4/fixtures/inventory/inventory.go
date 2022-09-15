43175575e036b71fee397269cbb416e0cbb99bdb
Roger Coll <rogercoll@protonmail.com>
2022-09-15 16:00:28 +0200

    fix: use only standard libraries
    module pulling in macos can be slow

--- internal/integrations/v4/fixtures/inventory/inventory.go
+++ internal/integrations/v4/fixtures/inventory/inventory.go
@@ -3,51 +3,30 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
-
-	"github.com/newrelic/infrastructure-agent/pkg/entity"
-	"github.com/newrelic/infrastructure-agent/pkg/integrations/v4/protocol"
 )
 
 // This fixture integration submits as inventory all the key=value pairs passed as arguments
 // It uses a remote entity.
 
+var inventoryTestSample = `{"name":"testing_integration","protocol_version":"2","integration_version":"1.2.3","integration_status":"","data":[{"entity":{"name":"localtest","type":"test","id_attributes":[{"Key":"idkey","Value":"idval"}],"displayName":"","metadata":null},"metrics":[],"inventory":{"cliargs":%s},"events":[],"add_hostname":false,"cluster":"local-test","service":"test-service"}]}`
+
 func main() {
-	inventory := protocol.InventoryData{}
-	for _, pair := range os.Args[1:] {
+	cliargs := "{"
+	for i, pair := range os.Args[1:] {
 		kv := strings.Split(pair, "=")
 		if len(kv) < 2 {
 			_, _ = fmt.Fprint(os.Stderr, "argument must be in form key=value. Got:", pair)
 			os.Exit(-1)
 		}
-		inventory[kv[0]] = kv[1]
+		cliargs += fmt.Sprintf("\"%s\":\"%s\"", kv[0], kv[1])
+		if i != len(os.Args[1:])-1 {
+			cliargs += ","
+		}
 	}
-	data := protocol.PluginDataV3{
-		PluginOutputIdentifier: protocol.PluginOutputIdentifier{
-			Name:               "testing_integration",
-			RawProtocolVersion: "2",
-			IntegrationVersion: "1.2.3",
-		},
-		DataSets: []protocol.PluginDataSetV3{{
-			Cluster: "local-test",
-			Service: "test-service",
-			PluginDataSet: protocol.PluginDataSet{Entity: entity.Fields{
-				Name:         "localtest",
-				Type:         "test",
-				IDAttributes: []entity.IDAttribute{{"idkey", "idval"}},
-			}, Inventory: map[string]protocol.InventoryData{
-				"cliargs": inventory,
-			}, Metrics: []protocol.MetricData{},
-				Events: []protocol.EventData{}},
-		}},
-	}
-	bytes, err := json.Marshal(data)
-	if err != nil {
-		_, _ = fmt.Fprint(os.Stderr, "can't write JSON:", err.Error())
-		os.Exit(-1)
-	}
-	fmt.Println(string(bytes))
+	cliargs += "}"
+
+	fmt.Println(fmt.Sprintf(inventoryTestSample, cliargs))
 }
